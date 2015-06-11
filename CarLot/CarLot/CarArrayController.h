80fc6e29a81637988dea785ab8c89f44379c2b40
Winter Zhong <zhongwm.net@gmail.com>
2015-06-12 07:51:22 +0800

    Binding Added.

---
+++ CarLot/CarLot/CarArrayController.h
@@ -0,0 +1,13 @@
+//
+//  CarArrayController.h
+//  CarLot
+//
+//  Created by Wunmin on 15/6/12.
+//  Copyright (c) 2015年 Wunmin Chung. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface CarArrayController : NSArrayController
+
+@end
---
+++ CarLot/CarLot/CarArrayController.m
@@ -0,0 +1,13 @@
+//
+//  CarArrayController.m
+//  CarLot
+//
+//  Created by Wunmin on 15/6/12.
+//  Copyright (c) 2015年 Wunmin Chung. All rights reserved.
+//
+
+#import "CarArrayController.h"
+
+@implementation CarArrayController
+
+@end
