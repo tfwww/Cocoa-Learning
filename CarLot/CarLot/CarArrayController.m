57c94a47158f7062c4c0401882aa801da7f87832
Winter Zhong <zhongwm.net@gmail.com>
2015-06-12 08:14:35 +0800

    Done challenge.

--- CarLot/CarLot/CarArrayController.m
+++ CarLot/CarLot/CarArrayController.m
@@ -10,4 +10,12 @@
 
 @implementation CarArrayController
 
+- (id)newObject {
+    
+    id newObj = [super newObject];
+    NSDate *now = [NSDate date];
+    [newObj setValue:now forKey:@"datePurchased"];
+    
+    return newObj;
+}
 @end
